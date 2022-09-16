6ce9b0b6c42e4637af438ce91f1742e33426a8ab
momoisgoodforhealth <shoban.shakya@gmail.com>
2022-09-16 14:04:26 -0400

    upload txt to file fix frr

--- upload.py
+++ upload.py
@@ -60,8 +60,10 @@ def upload_file():
                 #textfile2 = open(processed_text+".txt", "w")
                 #textfile2.write(str(count))
            
-                shutil.copy('/home/momoisgoodforhealth/Flask_upload/b.txt', '/home/momoisgoodforhealth/Flask_upload/'+folder+processed_text+".txt")
-                textfile2 = open("/home/momoisgoodforhealth/Flask_upload/"+folder+processed_text+".txt", "w")
+                #shutil.copy('/home/momoisgoodforhealth/Flask_upload/b.txt', '/home/momoisgoodforhealth/Flask_upload/'+folder+processed_text+".txt")
+                #textfile2 = open("/home/momoisgoodforhealth/Flask_upload/"+folder+processed_text+".txt", "w")
+                shutil.copy('/home/momoisgoodforhealth/Flask_upload/b.txt', UPLOAD_FOLDER2+processed_text+".txt")
+                textfile2 = open(UPLOAD_FOLDER2+processed_text+".txt", "w")
                 textfile2.write(processed_text)
                 #return redirect(url_for('download_file', name=filename))
 
